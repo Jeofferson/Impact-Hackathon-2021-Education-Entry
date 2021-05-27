51fc81151e7098320689a03cd5a9a5e62a56c046
Jeofferson Dela Peña <jeoffersond@gmail.com>
2021-05-28 00:08:53 +0800

    Added Career Result activity

---
+++ app/src/main/java/com/jeoffersondelapena/impacthackathon2021educationentry/ui/determinecareer/CareerResult.java
@@ -0,0 +1,16 @@
+package com.jeoffersondelapena.impacthackathon2021educationentry.ui.determinecareer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jeoffersondelapena.impacthackathon2021educationentry.R;
+
+public class CareerResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_career_result);
+    }
+}
